4e270294825b4295822935366c208315f35dbc29
Naoto Inoue <naoto.inoue.0804@gmail.com>
2024-10-30 10:23:37 +0000

    parallel executution

--- src/opencole/inference_launcher/gpt4_augmented_baselines.py
+++ src/opencole/inference_launcher/gpt4_augmented_baselines.py
@@ -20,8 +20,13 @@ def main() -> None:
     parser.add_argument("--output_dir", type=str, required=True)
     parser.add_argument("--first_n", type=int, default=None)
     parser.add_argument("--split_name", type=str, default="designerintention_v1")
+    parser.add_argument("--num_machines", type=int, default=1)
+    parser.add_argument("--machine_index", type=int, default=0)
     args, _ = parser.parse_known_args()
     logger.info(f"{args=}")
+
+    assert args.num_machines > 0
+    assert 0 <= args.machine_index < args.num_machines
 
     tester_class = t2itester_factory(args.tester)
     output_dir = Path(args.output_dir)
@@ -41,6 +46,9 @@ def main() -> None:
         output_path = output_dir / f"{input_.id}.png"
         if output_path.exists():
             logger.info(f"Skipped because {output_path=} exists.")
+            continue
+
+        if i % args.num_machines != args.machine_index:
             continue
 
         logger.info(f"{input_.id=} {input_.gpt_aug_prompt=}")
